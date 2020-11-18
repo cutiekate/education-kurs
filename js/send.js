979c271829934e41f4a55993436105741a02c799
cutiekate <cutiekate@outlook.com>
2020-11-18 10:35:20 +0200

    done

--- js/send.js
+++ js/send.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
         $('.tel-span-1').css('display', 'inline');
     });
     $('#phone-2').on('click', function() {
-        var countryData = iti.getSelectedCountryData().dialCode;
+        var countryData = iti2.getSelectedCountryData().dialCode;
         $('#prefix-2').val(countryData);
         if($('#phone-2').val()==""){
             $(this).val("+"+countryData);
@@ -36,7 +36,7 @@ $(document).ready(function () {
         $('.tel-span-2').css('display', 'inline');
     });
     $('#phone-3').on('click', function() {
-        var countryData = iti.getSelectedCountryData().dialCode;
+        var countryData = iti3.getSelectedCountryData().dialCode;
         $('#prefix-3').val(countryData);
         if($('#phone-3').val()==""){
             $(this).val("+"+countryData);
@@ -78,6 +78,8 @@ $(document).ready(function () {
         validMsg2 = document.querySelector("#valid-msg-2"),
         validMsg3 = document.querySelector("#valid-msg-3"),
         errorMap = ["Ошибка в номере", "Неправильный код страны", "Продолжайте ввод...", "Слишком много символов", "Продолжайте ввод..."],
+
+
         iti = window.intlTelInput(input, {
             initialCountry: "auto",
             defaultCountry: 'auto',
@@ -90,6 +92,7 @@ $(document).ready(function () {
             },
             utilsScript: "../js/utils.js"
         }),
+
         iti2 = window.intlTelInput(input2, {
             initialCountry: "auto",
             defaultCountry: 'auto',
@@ -118,12 +121,8 @@ $(document).ready(function () {
         btn = $('button.sub');
 
         var reset = function() {
-            input.classList.remove("error");
             input2.classList.remove("error");
             input3.classList.remove("error");
-            errorMsg.innerHTML = "";
-            errorMsg.classList.add("hide");
-            validMsg.classList.add("hide");
             errorMsg2.classList.add("hide");
             validMsg2.classList.add("hide");
             errorMsg3.classList.add("hide");
